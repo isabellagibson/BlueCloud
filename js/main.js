e61644712aaf729ba3b6258045c90b3c610c509b
Izzy Gibson <izzyyg27@gmail.com>
2022-08-08 14:18:47 -0700

    Update files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var termModal;
+var termModal, wasLastShown;
 
 setTimeout(() => {
     let priceBookLink = document.evaluate('/html/body/div[4]/div[1]/section/div[1]/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/div/div/div[1]/div/div/lst-list-view-manager-header/div/div[1]/div[3]/div/ul/li/a', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue;
@@ -19,7 +19,14 @@ setTimeout(() => {
 }, 3000);
 
 document.addEventListener('keyup', (e) => {
-    if (e.key === '`') {
+    if (e.key === 'F1' && e.ctrlKey) {
+        if (wasLastShown) {
+            termModal.style = 'visibility: hidden; display: none;';
+            wasLastShown = false;
+        } else {
+            termModal.style = 'visibility: visible; display: block;';
+            wasLastShown = true;
+        }
         alert('Interrupting.');
     } else {
         console.log(e.key);
