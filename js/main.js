d92cbe9b795a54f135fdc120ea1afda85d65f7a9
Izzy Gibson <izzyyg27@gmail.com>
2022-08-08 13:59:30 -0700

    Add preliminary files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var termModal;
+
+setTimeout(() => {
+    let priceBookLink = document.evaluate('/html/body/div[4]/div[1]/section/div[1]/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/div/div/div[1]/div/div/lst-list-view-manager-header/div/div[1]/div[3]/div/ul/li/a', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue;
+    // Add custom stylesheet
+    let css = document.createElement('link');
+    css.setAttribute('rel', 'stylesheet');
+    css.setAttribute('href', '')
+    // Basic structure
+    termModal = document.createElement('div');
+    termModal.id = 'termModal';
+    // Modal content
+    modal = document.createElement('div');
+    modal.className = 'modal';
+    modal.innerHTML = 'Hello, world!';
+    termModal.appendChild(modal);
+    document.body.appendChild(termModal);
+}, 5000);
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === '`') {
+        alert('Interrupting.');
+    } else {
+        console.log(e.key);
+    }
+});
